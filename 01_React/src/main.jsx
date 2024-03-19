c931f85641d89a4755877f25ab1b707a1685ca81
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-03-19 11:11:52 +0530

    Created a custom React working model

--- 01_React/src/main.jsx
+++ 01_React/src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+const customElement = React.createElement(
+    'a',
+    {
+      href: 'https://google.com',
+      target: '_blank'
+    },
+    'google.com'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  customElement
+  // <App />
 )
