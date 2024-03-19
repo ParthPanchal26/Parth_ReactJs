3c30d7c942a9888781101b7aaad85814ca826a6d
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-03-19 10:03:26 +0530

    First React prc

---
+++ 01_React/src/App.jsx
@@ -0,0 +1,13 @@
+import Parth from "./parth"
+
+function App() {
+
+  return (
+    <>
+      <Parth />
+      <h1>Hi! from App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01_React/src/parth.jsx
@@ -0,0 +1,7 @@
+const Parth = () => {
+    return ( 
+        <h1>Hey! Parth is here!</h1>
+     );
+}
+ 
+export default Parth;
